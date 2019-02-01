7c3edff0d74b53a1ecc6780fddf140ceda43e54d
allynz <43417311+allynz@users.noreply.github.com>
2019-02-01 18:57:47 +0530

    Update AGGRCOW.cpp

--- SPOJ/AGGRCOW.cpp
+++ SPOJ/AGGRCOW.cpp
@@ -43,8 +43,8 @@ void solve(){
 }
  
 int main(){
-    
-    ios::sync_with_stdio(0) ; cin.tie(0) ; cout.tie(0) ;
+	
+	ios::sync_with_stdio(0) ; cin.tie(0) ; cout.tie(0) ;
 	
 	ll t ; cin >> t ;
 	while(t--) solve() ;
